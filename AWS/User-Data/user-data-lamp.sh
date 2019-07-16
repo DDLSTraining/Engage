041bbb98277e7449b98f07e72e2b4e39d8366c20
Grant Carthew <grant@carthew.net>
2019-07-16 18:42:13 +1000

    Create user-data-lamp.sh

---
+++ AWS/User-Data/user-data-lamp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
