98a74d4aa75815ad6ef61b43b44cbd2d3c2f537b
Grant Carthew <grant@carthew.net>
2020-03-23 14:06:47 +1000

    Update Get-AvailableSku.ps1

--- Azure/Get-AvailableSku.ps1
+++ Azure/Get-AvailableSku.ps1
@@ -1,4 +1,4 @@
 $allSku = Get-AzComputeResourceSku
 $noRestrictionsSku = $allSku | Where-Object { $_.ResourceType -eq "virtualMachines" -and $_.Restrictions.length -lt 1 }
-$availableSkuSummary = $noRestrictionsSku | Select-Object -Property Locations,Name
+$availableSkuSummary = $noRestrictionsSku | Foreach-Object { [PSCustomObject]@{ Location=[String]$_.Locations; Name=$_.Name } }
 Return $availableSkuSummary
