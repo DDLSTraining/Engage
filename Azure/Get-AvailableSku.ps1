40ff6a2c04027e4a24ad7d38d62585cf4cdec7d9
Grant Carthew <grant.carthew@ddls.com.au>
2020-03-24 17:51:31 +1000

    Added sku parameter

--- Azure/Get-AvailableSku.ps1
+++ Azure/Get-AvailableSku.ps1
@@ -1,28 +1,49 @@
 <#
 .SYNOPSIS
-  Returns a list of available virtual machine sizes and locations.
+  Lists available virtual machine sizes and locations for your subscription in Azure.
 .DESCRIPTION
-  Filters out virtual machine size SKUs that are not available.
+  The Azure platform sometimes restricts which virtual machine stock code units, or
+  sizes of machines, are available for different subscriptions.
+
+  This script lists all the available virtual machine sizes (SKUs) that are
+  available for your subscription in each location.
 .EXAMPLE
+  The following example lists all available virtual machine SKU and locations.
+  Get-AvailableSku
+.EXAMPLE
+  This example will list only the SKUs for location 'australiaeast'.
   Get-AvailableSku -location australiaeast
 .EXAMPLE
+  This example will list only the locations for SKU 'Standard_D1'.
+  Get-AvailableSku -SKU Standard_D1
+.EXAMPLE
+  This example groups the output by location showing the count of
+  available SKUs in each location.
+  Get-AvailableSku -GroupByLocation
+.EXAMPLE
+  This example groups the output by available SKUs showing the count of
+  the locations.
   Get-AvailableSku -GroupByLocation
 .PARAMETER Location
   Filters the output to just the required location.
+.PARAMETER Sku
+  Filters the output to just the required stock code unit or size.
 .PARAMETER GroupByLocation
   Aggregates the locations showing the count of available SKUs.
 .PARAMETER GroupBySku
   Aggregates the available SKUs showing the count by location.
 #>
 
-[CmdletBinding(DefaultParameterSetName='All')]
-[Alias('gas')]
+[CmdletBinding(DefaultParameterSetName='Location')]
 [OutputType([Array])]
 Param (
-  [Parameter(ParameterSetName='All')]
-  [Parameter(Mandatory=$False, ParameterSetName = 'Filter')]
+  [Parameter(Mandatory=$False, ParameterSetName = 'Location')]
   [String]
   $Location = 'All',
+
+  [Parameter(Mandatory=$False, ParameterSetName = 'Sku')]
+  [String]
+  $Sku,
 
   [Parameter(Mandatory=$False, ParameterSetName = 'GroupByLocation')]
   [Switch]
@@ -35,20 +56,27 @@ Param (
 
 
 $allSku = Get-AzComputeResourceSku
+$vmSku = $allSku | Where-Object {
+  $_.ResourceType -eq "virtualMachines" -and $_.Restrictions.length -lt 1
+} | Sort-Object -Property Locations
+
 if ($Location -ne 'All') {
-  $noRestrictionsSku = $allSku | Where-Object { $_.ResourceType -eq "virtualMachines" `
-    -and $_.Restrictions.length -lt 1 `
-    -and [String]$_.Location -eq $Location }
-} else {
-  $noRestrictionsSku = $allSku | Where-Object { $_.ResourceType -eq "virtualMachines" `
-    -and $_.Restrictions.Length -lt 1 }
+  $vmSku = $vmSku | Where-Object { [String]$_.Locations -eq $Location } | `
+    Sort-Object -Property Name
+} elseif ($Sku) {
+  $vmSku = $vmSku | Where-Object { $_.Name -eq $Sku }
 }
-$availableSkuSummary = $noRestrictionsSku | Foreach-Object { [PSCustomObject]@{ Location=[String]$_.Locations; SKU=$_.Name } }
+
+$result = $vmSku | Foreach-Object {
+  [PSCustomObject]@{ Location=[String]$_.Locations; SKU=$_.Name }
+}
 
 if ($GroupByLocation) {
-  Return $availableSkuSummary | Group-Object -Property Location
+  $result = $result | Group-Object -Property Location | `
+    Select-Object -Property Count,Name | Sort-Object -Property Count
 } elseif ($GroupBySku) {
-  Return $availableSkuSummary | Group-Object -Property SKU
-} else {
-  Return $availableSkuSummary
+  $result = $result | Group-Object -Property SKU | `
+    Select-Object -Property Count,Name | Sort-Object -Property Count
 }
+
+Return $result
